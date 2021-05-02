c9010a3bded10a020f9eaa6726195dd3912eb136
rkodev <43806892+rkodev@users.noreply.github.com>
2021-05-02 16:19:20 +0300

    Move tests to better folder

--- app/src/androidTest/java/com/roksky/sample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/roksky/sample/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.techprescribed.sales", appContext.packageName)
+        assertEquals("com.roksky.sales", appContext.packageName)
     }
 }
